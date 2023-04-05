46e475ef1bb6b74fa368ee3ec631a374f378bcd9
Shakil <theshakil53@gmail.com>
2023-04-05 10:29:23 +0600

    testing purpose for git branching

--- index.js
+++ index.js
@@ -8,13 +8,14 @@
 // dependencies
 const http = require('http');
 const url = require('url');
+const {StringDecoder} = require('string_decoder')
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-    port: 3000,
+    port: 8000,
 };
 
 // create server
@@ -29,13 +30,29 @@ app.createServer = ()=>{
 app.handleReqRes=(req,res)=>{
     // handle req
     // get the url and parsed it
-    const parsedUrl = url.parse(req.url, true)
-    const path = parsedUrl.pathname
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
-    console.log(trimmedPath);
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.header;
+    
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
 
-    // handle Response
-    res.end('hello programers');
+    req.on('data', (buffer)=>{
+        realData += decoder.write(buffer)
+    })
+
+    req.on('end',()=>{
+        realData += decoder.end();
+        console.log(realData);
+
+        // handle Response
+        res.end('hello programers');
+    })
+
+
 };
 
 // start server
