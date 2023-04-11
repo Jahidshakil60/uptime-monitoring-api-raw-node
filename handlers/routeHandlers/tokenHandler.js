ca30bacf53d020ab5c1c6f256a2d20e64534506b
Shakil <theshakil53@gmail.com>
2023-04-11 13:25:22 +0600

    create sessoin base token

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -6,7 +6,8 @@
 //
 //dependencies
 const data = require("../../lib/data");
-// const { hash, parseJSON } = require("../../helpers/utilities");
+const { hash, parseJSON } = require("../../helpers/utilities");
+const {createRandomString} = require('../../helpers/utilities')
 
 // module scaffolding
 const handler = {};
@@ -24,8 +25,54 @@ handler._token = {};
 
 handler._token.get = (requestProperties,callback)=>{};
 
-handler._token.post = (requestProperties,callback)=>{};
+handler._token.post = (requestProperties,callback)=>{
+    const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
+
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password
+      : false;
+
+      if(phone && password){
+        data.read('users',phone, (err1, userData)=>{
+            let hashedpassword = hash(password)
+            if(hashedpassword === parseJSON(userData).password){
+                let tokenId = createRandomString(20);
+                let expires = Date.now() + 60 *60 * 1000;
+                let tokenObject = {
+                     phone,
+                    'id': tokenId,
+                    expires
+                }
+                data.create('tokens', tokenId , tokenObject ,(err2)=>{
+                    if(!err2){
+                        callback(200, tokenObject);
+                    }else{
+                        callback(500,{
+                            error: 'there was a problem in the server side'
+                        })
+                    }
+                })
+            }else{
+                callback(400, {
+                    error: "password is not valid",
+                  });
+            }
+        })
+      }else{
+        callback(400, {
+            error: "You have a problem in your request",
+          });
+      }
+};
 
 handler._token.put = (requestProperties,callback)=>{};
 
 handler._token.delete = (requestProperties,callback)=>{};
+
+module.exports = handler;
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -1,40 +1,57 @@
-// 
-// Title: Utilities 
-// Description: Handle all utilities related things 
+//
+// Title: Utilities
+// Description: Handle all utilities related things
 // Author: Jahid Hasan Shakil
 // Date: 06/03/2023
-// 
+//
 
 //dependencies
-const crypto = require('crypto');
-const environments = require('./environments')
+const crypto = require("crypto");
+const environments = require("./environments");
 
 // module scaffolding
 const utilities = {};
 
 //parsejson string to object
-utilities.parseJSON = (jsonString)=>{
-    let output ;
-    try {
-        output= JSON.parse(jsonString)
-    } catch {
-        output={}
-    }
-    return output;
-}
+utilities.parseJSON = (jsonString) => {
+  let output;
+  try {
+    output = JSON.parse(jsonString);
+  } catch {
+    output = {};
+  }
+  return output;
+};
 
 //hash string
-utilities.hash = (str)=>{
-    if (typeof(str)==='string' && str.length>0){
-        const hash = crypto
-        .createHmac('sha256', environments.secretKey )
-        .update(str)
-        .digest('hex');
-        return hash;
-    }else{
-        return false;
-    }
+utilities.hash = (str) => {
+  if (typeof str === "string" && str.length > 0) {
+    const hash = crypto
+      .createHmac("sha256", environments.secretKey)
+      .update(str)
+      .digest("hex");
+    return hash;
+  } else {
+    return false;
+  }
+};
 
-}
+//create random string
+utilities.createRandomString = (strLength) => {
+  let length = strLength;
+  length = typeof strLength === "number" && strLength > 0 ? strLength : false;
+  if(length){
+    let possiblecharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+     let output = '';
+     for(let i=1; i<= length;i++){
+        const randomCharecter = possiblecharacters.charAt(Math.floor(Math.random() * possiblecharacters.length))
+        output += randomCharecter;
+     }
+     return output;
+     
+  }else{
+    return false;
+  }
+};
 
 module.exports = utilities;
